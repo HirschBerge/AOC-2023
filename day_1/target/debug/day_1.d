8ccbd0887ef71c15c5405418ed6c629041eba801
HirschBerge <hskirkwo@geneva.edu>
2023-11-30 10:49:48 -0500

    Setting up for Day 1

---
+++ day_1/target/debug/day_1.d
@@ -0,0 +1,1 @@
+/home/hirschy/projects/AOC-2023/day_1/target/debug/day_1: /home/hirschy/projects/AOC-2023/day_1/src/main.rs
---
+++ day_1/target/debug/deps/day_1-b801a19a27e42161.d
@@ -0,0 +1,5 @@
+/home/hirschy/projects/AOC-2023/day_1/target/debug/deps/day_1-b801a19a27e42161: src/main.rs
+
+/home/hirschy/projects/AOC-2023/day_1/target/debug/deps/day_1-b801a19a27e42161.d: src/main.rs
+
+src/main.rs:
