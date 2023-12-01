50de20f666612393551229e300a7ae2a930a1dbd
HirschBerge <hskirkwo@geneva.edu>
2023-12-01 12:32:49 -0500

    Part 2 was rough, lol.

---
+++ day_1/README.md
@@ -0,0 +1,8 @@
+# This one was kinda tough
+
+## Part 1 was pretty easy, but part two, I just couldn't.
+I was using keys in a HashMap to regex the key to the variable. I had it working until I realized that this problem didn't make sense in context of the "story." Mixed alphabetical numbers and you had to accept BOTH of them!? Preposterous!
+
+Well, that stumped me. When I added hacky shit like `twone` and `sevenine` the real numbers stopped registering!. So since I have nothing to prove, I figured it would be better to see someone's code that actually works and learn from that.
+
+I ended up ~~stealing~~ recreating [WilkoTom's](https://github.com/wilkotom/Aoc2023/blob/main/day01/src/main.rs) method for part 2. Very cool solution. Similar to what I was going for, just... Well with experience.
--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,35 +1,125 @@
-use std::fs::read_to_string;
+use aochelpers::get_daily_input;
+use std::error::Error;
 
-fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
+const WORDVALUES: [(&str, i32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
 
-    for line in read_to_string(filename).unwrap().lines() {
-        result.push(line.to_string())
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = get_daily_input(1, 2023)?;
+    println!("Part 1 result: {}", answer(&data, false));
+    println!("Part 2 result: {}", answer(&data, true));
+
+    Ok(())
+}
+
+fn answer(data: &str, part2: bool) -> i32 {
+    let mut result = 0;
+    for line in data.split('\n') {
+        result += first_number(line, part2) * 10 + last_number(line, part2)
     }
-
     result
 }
-fn main() {
-    let output = read_lines("input.txt");
-    let mut sum = 0;
-    for line in output {
-        let last = line
-            .chars()
-            .filter(|c| c.is_numeric())
-            .last()
-            .to_owned()
-            .unwrap()
-            .to_string();
-        let first = line
-            .chars()
-            .filter(|c| c.is_numeric())
-            .nth(0)
-            .to_owned()
-            .unwrap()
-            .to_string();
-        let digis = format!("{}{}", first, last).parse().unwrap_or(0);
-        sum += digis;
-        // println!("Running total: {}", sum);
+
+fn first_number(s: &str, part2: bool) -> i32 {
+    if s.is_empty() {
+        0
+    } else if let Some(v) = s.chars().next().unwrap_or('z').to_digit(10) {
+        v as i32
+    } else if let Some(v) = WORDVALUES.iter().find_map(|(w, v)| {
+        if part2 && s.starts_with(w) {
+            Some(v)
+        } else {
+            None
+        }
+    }) {
+        *v
+    } else {
+        first_number(&s[1..], part2)
     }
-    println!("Total: {}", sum)
+}
+
+fn last_number(s: &str, part2: bool) -> i32 {
+    if s.is_empty() {
+        0
+    } else if let Some(v) = s.chars().last().unwrap_or('z').to_digit(10) {
+        v as i32
+    } else if let Some(v) = WORDVALUES.iter().find_map(|(w, v)| {
+        if part2 && s.ends_with(w) {
+            Some(v)
+        } else {
+            None
+        }
+    }) {
+        *v
+    } else {
+        last_number(&s[..s.len() - 1], part2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_first_number_part1() {
+        assert_eq!(first_number("1abc2", false), 1);
+        assert_eq!(first_number("pqr3stu8vwx", false), 3);
+        assert_eq!(first_number("a1b2c3d4e5f", false), 1);
+        assert_eq!(first_number("treb7uchet", false), 7);
+    }
+    #[test]
+    fn test_last_number_part1() {
+        assert_eq!(last_number("1abc2", false), 2);
+        assert_eq!(last_number("pqr3stu8vwx", false), 8);
+        assert_eq!(last_number("a1b2c3d4e5f", false), 5);
+        assert_eq!(last_number("treb7uchet", false), 7);
+    }
+    #[test]
+    fn test_first_number_part2() {
+        assert_eq!(first_number("two1nine", true), 2);
+        assert_eq!(first_number("eightwothree", true), 8);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("xtwone3four", true), 2);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("4nineeightseven2", true), 4);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("zoneight234", true), 1);
+        assert_eq!(first_number("abcone2threexyz", true), 1);
+        assert_eq!(first_number("7pqrstsixteen", true), 7);
+    }
+
+    #[test]
+    fn test_part1() {
+        let data: &str = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let part1_res = answer(data, false);
+        assert_eq!(part1_res, 142);
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let part1_res = answer(data, true);
+        assert_eq!(part1_res, 281);
+    }
 }
