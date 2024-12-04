b2be8714ad891ecc865038e0ff15bdb77baa9714
HirschBerge <hirschy.kirkwood@proton.me>
2024-12-03 23:54:41 -0500

    added in a really stupid and slow brute force method just as a benchmark
    yes... it works (for part one, f part 2 for that)

--- day_3/benches/aoc.rs
+++ day_3/benches/aoc.rs
@@ -14,3 +14,22 @@ fn bench_part1() {
 fn bench_part2() {
     part2();
 }
+
+#[divan::bench]
+fn bench_lolz() {
+    let file = include_str!("../../.inputs/2024/3");
+    let mut answer = 0;
+
+    for i in 1..1000 {
+        for j in 1..1000 {
+            let pattern = format!("mul({},{})", i, j);
+            let times_seen: Vec<&str> = file.matches(pattern.as_str()).collect();
+            let times = times_seen.len();
+
+            if times != 0 {
+                answer += times * i * j
+            }
+        }
+    }
+    println!("Your answer is: {}", answer);
+}
