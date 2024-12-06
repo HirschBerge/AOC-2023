baba17172733522eb63a287a953341a72fde92a3
HirschBerge <hirschy.kirkwood@proton.me>
2024-12-06 07:54:04 -0500

    day6 to begin

---
+++ day_6/src/part1.rs
@@ -0,0 +1,16 @@
+use crate::get_daily_input;
+
+pub fn part1() -> String {
+    let data = get_daily_input(2024, 6);
+    data
+}
+
+#[cfg(test)]
+mod tests {
+    // use crate::*;
+
+    #[test]
+    fn test_one() {
+        assert_eq!(1, 1);
+    }
+}
---
+++ day_6/src/part2.rs
@@ -0,0 +1,15 @@
+use crate::get_daily_input;
+
+pub fn part2() -> String {
+    get_daily_input(2024, 6)
+}
+
+#[cfg(test)]
+mod tests {
+    // use crate::*;
+
+    #[test]
+    fn test_one() {
+        assert_eq!(1, 1);
+    }
+}
