b6e482621c00cf3bcf2120776c1faeeaaa2bce7c
HirschBerge <hirschy.kirkwood@proton.me>
2024-12-11 06:45:34 -0500

    day complete.
    thank you to this commentor for helping me understand how to implement
    recursion properly
    https://www.reddit.com/r/adventofcode/comments/1hbm0al/comment/m1i72yv/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- 2024/day_11/src/lib.rs
+++ 2024/day_11/src/lib.rs
@@ -10,6 +10,48 @@ pub mod part1;
 pub mod part2;
 use std::str::FromStr;
 
+use std::collections::HashMap;
+
+use nom::character;
+pub fn score_stone(stone: i64, depth: u8, cachemap: &mut HashMap<(i64, u8), i64>) -> i64 {
+    if depth == 0 {
+        return 1;
+    }
+    if let Some(&cached_result) = cachemap.get(&(stone, depth)) {
+        return cached_result;
+    }
+    let str_stone = stone.to_string();
+    let result = if stone == 0 {
+        score_stone(1, depth - 1, cachemap)
+    } else if str_stone.len() % 2 == 0 {
+        let digits = (stone as f64).log10().floor() as u32 + 1;
+        let divisor = 10i64.pow(digits / 2);
+
+        let left = stone / divisor;
+        let right = stone % divisor;
+        score_stone(left, depth - 1, cachemap) + score_stone(right, depth - 1, cachemap)
+    } else {
+        return score_stone(stone * 2024, depth - 1, cachemap);
+    };
+
+    cachemap.insert((stone, depth), result);
+    result
+}
+
+// OPTIMIZE: Sort, cache answers, get count of repeated numbers, for i in repeated.len() vec.push()
+pub fn score_all_stones(stones: Vec<i64>, blinks: u8) -> i64 {
+    let mut lookup: HashMap<(i64, u8), i64> = HashMap::new();
+    let mut answer: i64 = 0;
+    for stone in stones {
+        answer += score_stone(stone, blinks, &mut lookup);
+    }
+    answer
+}
+
+fn parsing(input: &str) -> IResult<&str, Vec<i64>> {
+    separated_list1(space1, character::complete::i64)(input)
+}
+
 pub fn get_daily_input(year: i32, day: i32) -> String {
     let path = format!("../.inputs/{}/{}", year, day);
     match std::fs::read_to_string(path) {
--- 2024/day_11/src/part1.rs
+++ 2024/day_11/src/part1.rs
@@ -1,14 +1,24 @@
-pub fn part1() -> &'static str {
+use crate::{parsing, score_all_stones};
+
+pub fn part1() -> i64 {
     let data = include_str!("../input");
-    data
+    let (_, parsed_stones) = parsing(data).unwrap();
+    score_all_stones(parsed_stones, 25)
 }
+
+// TODO: Return the score for a given stone in a manner that is easy to cache.
 
 #[cfg(test)]
 mod tests {
     // use crate::*;
 
+    use crate::{parsing, score_all_stones};
+
     #[test]
     fn test_one() {
-        assert_eq!(1, 1);
+        let input = "125 17";
+        let (_, parsed_stones) = parsing(input).unwrap();
+        let ans = score_all_stones(parsed_stones, 25);
+        assert_eq!(55312, ans);
     }
 }
