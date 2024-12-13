e20ac1f6adece47d7ee0459a9e311fd2c3d7b7bb
HirschBerge <hirschy.kirkwood@proton.me>
2024-12-13 13:01:35 -0500

    day 13

---
+++ 2024/day_13/src/part1.rs
@@ -0,0 +1,14 @@
+pub fn part1() -> &'static str {
+    let data = include_str!("../input");
+    data
+}
+
+#[cfg(test)]
+mod tests {
+    // use crate::*;
+
+    #[test]
+    fn test_one() {
+        assert_eq!(1, 1);
+    }
+}
